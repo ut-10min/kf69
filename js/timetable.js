fbadcfb7493c53069626e95463fec14278f6e201
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2018-11-02 15:16:26 +0900

    2017駒場祭のコピー

---
+++ js/timetable.js
@@ -0,0 +1,34 @@
+function construstTimeTable(timeTable, talksData) {
+  return Object.keys(timeTable)
+               .filter(function (k) {return timeTable[k];})
+               .sort()
+               .map(function (time) {
+                 var name = timeTable[time];
+                 var index = 0;
+
+                 if (name == "なおと") {
+                   name = "宇佐美尚人";
+                 } else if (name.indexOf("こすも") == 0) {
+                   index = parseInt(name.charAt(3)) - 1;
+                   name = "宇佐美こすも";
+                 }
+
+                 var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
+
+                 return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };
+               });
+}
+
+
+$(function () {
+  var firstDayTable = construstTimeTable(day1, data);
+  var secondDayTable = construstTimeTable(day2, data);
+  var thirdDayTable = construstTimeTable(day3, data);
+
+  var template = $('#template').html();
+  Mustache.parse(template);
+  var renderedFirst = Mustache.render(template, {table: firstDayTable, header: "11/24(金)"});
+  var renderedSecond = Mustache.render(template, {table: secondDayTable, header: "11/25(土)"});
+  var renderedThird = Mustache.render(template, {table: thirdDayTable, header: "11/26(日)"});
+  $('.article-headline').html(renderedFirst + "<br />" + renderedSecond + "<br />" + renderedThird);
+});
