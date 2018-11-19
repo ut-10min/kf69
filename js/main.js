ef842b749c83f1c454c03867811fd25ea27f0eac
Hajime Fukuda <hajime.fukuda@me.com>
2018-11-19 13:08:01 +0900

    added anchor

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(function() {
   Mustache.parse(template);
   var rendered = Mustache.render(template, {talks: data});
   $('.article-headline').html(rendered);
+
+  var hash_temp = location.hash;
+  if (hash_temp) {
+    location.replace('#');
+    location.replace(hash_temp);
+  }
 });
